cd034c60fda5998f736a167534c12eedc637be61
hnjog <hnjogo@gmail.com>
2023-11-17 20:53:49 +0900

    echo server test

---
+++ echoclient.c
@@ -0,0 +1,34 @@
+#include "csapp.h"
+
+int main(int argc, char **argv)
+{
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage : %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+
+    host = argv[1];
+    port = argv[2];
+
+    printf("make clientfd \n");
+    clientfd = Open_clientfd(host, port);
+    printf("set rio readinit \n");
+    Rio_readinitb(&rio, clientfd);
+    printf("waits fgets \n\n");
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        printf("in while....\n");
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
+    }
+
+    Close(clientfd);
+    exit(0);
+}
